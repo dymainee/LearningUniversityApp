881c92c885b35995ef409587294326f022819ed8
dymainee <dimastashkoe@gmail.com>
2026-03-21 16:30:35 +0000

    student

---
+++ LearningUniversityApp/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace LearningUniversityApp.Models
+{
+    public class Student
+    {
+        public int Id { get; }  
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public Student(){}
+        public Student(int id, string name,string surname, int age, DateOnly dateofbirth) {
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.DateOfBirth = dateofbirth;
+
+
+           
+            
+        }
+    }
+}
