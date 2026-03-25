324e8f811a3a26555c52edc6605d8e04e6469df2
dymainee <dimastashkoe@gmail.com>
2026-03-25 16:09:29 +0000

    added Data content

---
+++ LearningUniversityApp/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using LearningUniversityApp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace LearningUniversityApp.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().HasKey(s => s.Id);
+
+               
+        }
+       
+    }
+}
